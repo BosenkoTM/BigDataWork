d77c3d7b91e494c998a7e77c2d2afffe883fe2e7
BosenkoTM <38157538+BosenkoTM@users.noreply.github.com>
2022-06-01 22:46:44 +0300

    Delete exercises/winter_semester_2021-2022 directory

--- exercises/winter_semester_2021-2022/03_hive-ql_partitioning_hive-server/name_basics.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS name_basics (
-	nconst STRING, 
-	primary_name STRING, 
-	birth_year INT, 
-	death_year STRING, 
-	primary_profession STRING, 
-	known_for_titles STRING
-	) COMMENT 'IMDb Actors' ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE LOCATION '/user/hadoop/imdb/name_basics'
-TBLPROPERTIES ('skip.header.line.count'='1');
